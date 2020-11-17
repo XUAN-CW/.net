8ba6ae66b6d6e8fc6692d6fe4cbf995537bd53ba
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-17 13:53:12 +0800

    关键代码

---
+++ lab01/lab01/Controllers/MyIndexController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class MyIndexController : Controller
+    {
+        //
+        // GET: /MyIndex/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult doget()
+        {
+            ViewData["outputdata"] = Request["name"].ToString();
+            return View();
+        }
+    }
+}
