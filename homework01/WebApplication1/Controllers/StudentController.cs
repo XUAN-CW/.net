c4f338dc3095551fb5ce2e237f9e061081b49045
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-12-01 23:22:51 +0800

    homework01

---
+++ homework01/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private studentDBContext db = new studentDBContext();
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(db.students.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(T_Student s)
+        {
+            db.students.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string sno)
+        {
+            T_Student s = db.students.Find(sno);
+            if (s == null)
+            {
+                return HttpNotFound();
+            }
+            return View(s);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(T_Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(s).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(s);
+        }
+        public ActionResult Delete(string sno)
+        {
+            if (sno == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_Student s = db.students.Find(sno);
+            if (s == null)
+            {
+                return HttpNotFound();
+            }
+            return View(s);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult Delete1(string sno)
+        {
+            T_Student s = db.students.Find(sno);
+            db.students.Remove(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string sno)
+        {
+            if (sno == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_Student s = db.students.Find(sno);
+            if (s == null)
+            {
+                return HttpNotFound();
+            }
+            return View(s);
+        }
+    }
+}
---
+++ homework01/WebApplication1/Models/T_Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WebApplication1.Models
+{
+    public class T_Student
+    {
+        [Key]
+        [Required(ErrorMessage = "不能为空")]
+        [Display(Name = "学号")]
+        public string Sno { get; set; }
+
+        [Required(ErrorMessage = "不能为空")]
+        [Display(Name = "姓名")]
+        public string Sname { get; set; }
+
+        [Required(ErrorMessage = "不能为空")]
+        [Display(Name = "性别")]
+        public string Sex { get; set; }
+
+        [Required(ErrorMessage = "不能为空")]
+        [Display(Name = "qq号")]
+        public string SQQ { get; set; }
+
+        [Required(ErrorMessage = "不能为空")]
+        [Display(Name = "宿舍号")]
+        public string Sdormitory { get; set; }
+
+    }
+
+    public class studentDBContext : DbContext
+    {
+        [Key]
+        public DbSet<T_Student> students { get; set; }
+    }
+}
