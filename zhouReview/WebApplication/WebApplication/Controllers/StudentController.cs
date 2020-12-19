99d8a1b0a45cff0166a962b616f3be81045eb05f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-12-19 18:42:04 +0800

    周志兆的复习

---
+++ zhouReview/WebApplication/WebApplication/Controllers/StudentController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentDBContext db = new StudentDBContext();
+
+
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+  
+        public ActionResult Details(string id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+
+        public ActionResult Delete(string id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ zhouReview/WebApplication/WebApplication/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Student
+    {
+        [Display(Name = "id")]
+        [Required(ErrorMessage = "必填")]
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "必须是6~10个字符")]
+        public string id { get; set; }
+
+        [Display(Name = "name")]
+        [Required(ErrorMessage = "必填")]
+        [StringLength(10)]
+        public string name { get; set; }
+
+        [Display(Name = "sex")]
+        [Required(ErrorMessage = "必填")]
+        [StringLength(1)]
+        public string sex { get; set; }
+
+        [Display(Name = "age")]
+        [Required(ErrorMessage = "必填")]
+        [Range(1, 100)]//数据范围验证信息
+        public int age { get; set; }
+    }
+
+    //创建数据库上下文类，上下文类代码可以和Student类放在一起写
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
