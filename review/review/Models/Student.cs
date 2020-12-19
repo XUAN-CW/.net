0c9de09ad09f0bd61154f73c97c3c6d864385c50
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-12-19 18:30:14 +0800

    EF增删改查

---
+++ review/review/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace review.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public int age { get; set; }
+    }
+}
